95ac0cac638402a9f6fdab6565e12698f9d8bbec
arthur.gollmer <arthur.gollmer@googlemail.com>
2019-04-07 17:02:14 +0200

    Commit Test

--- my_script.py
+++ my_script.py
@@ -2,5 +2,5 @@ from pystrich.datamatrix import DataMatrixEncoder
 
 encoder = DataMatrixEncoder('This is a DataMatrix.')
 encoder.save('./datamatrix_test.png')
-print("Dockertest....")
+print("Dockertest now....")
 print(encoder.get_ascii())
