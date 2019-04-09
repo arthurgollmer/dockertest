d7b7ccddd06322b69d0a56855293854b6b7f1c12
arthur.gollmer <arthur.gollmer@googlemail.com>
2019-04-09 15:02:27 +0200

    Namenskorrektur: DockerFile --> Dockerfile

--- my_script.py
+++ my_script.py
@@ -2,5 +2,5 @@ from pystrich.datamatrix import DataMatrixEncoder
 
 encoder = DataMatrixEncoder('This is a DataMatrix.')
 encoder.save('./datamatrix_test.png')
-print("Dockertest alles neu....")
+print("Pipeline done....")
 print(encoder.get_ascii())
